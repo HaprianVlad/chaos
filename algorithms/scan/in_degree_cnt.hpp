f52405f106f047d43b829881eeac2a62c81cc265
VladH <vlad.haprian@epfl.ch>
2016-03-08 18:34:47 +0100

    fix on in degree

--- algorithms/scan/in_degree_cnt.hpp
+++ algorithms/scan/in_degree_cnt.hpp
@@ -137,8 +137,8 @@ namespace algorithm {
                                      unsigned long copy_machine,
                                      per_processor_data *per_cpu_data,
                                      unsigned long bsp_phase) {
-                struct degree_cnts_vertex *vtx = (struct degree_cnts_vertex *) v;
-                struct degree_cnts_vertex *vtx_cpy = (struct degree_cnts_vertex *) copy;
+                //struct degree_cnts_vertex *vtx = (struct degree_cnts_vertex *) v;
+                //struct degree_cnts_vertex *vtx_cpy = (struct degree_cnts_vertex *) copy;
                // vtx->degree += vtx_cpy->degree;
             }
 
