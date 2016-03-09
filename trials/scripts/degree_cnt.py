e79252852ecf121fa4c04ac0161b2926394b32a3
VladH <vlad.haprian@epfl.ch>
2016-03-09 20:05:20 +0100

    degree count script added

---
+++ trials/scripts/degree_cnt.py
@@ -0,0 +1,27 @@
+
+import sys
+import struct
+
+print 'Finding out and in degree in of first 10 vertices ' + sys.argv[1]
+
+maxID = 10
+outDegrees = {}
+inDegrees = {}
+for i in range(maxID):
+	outDegrees[i] = 0
+	inDegrees[i] = 0		
+
+
+with open(sys.argv[1],'rb') as infile:
+    
+	for chunk in iter((lambda:infile.read(8)),''):
+	        src = struct.unpack('I', chunk[0:4])[0]
+       	        tgt = struct.unpack('I', chunk[4:8])[0]
+	        if src < maxID:
+        		outDegrees[src] = outDegrees[src] + 1
+		if tgt < maxID:
+           		inDegrees[tgt] = inDegrees[tgt] + 1
+
+print "outDegrees=" + outDegrees
+print "inDegrees=" + outDegrees
+
