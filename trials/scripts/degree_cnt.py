e43a6fc48e08797238ea3580bd6461a6e649bc08
VladH <vlad.haprian@epfl.ch>
2016-03-11 09:59:47 +0100

    scripts udpated

--- trials/scripts/degree_cnt.py
+++ trials/scripts/degree_cnt.py
@@ -10,18 +10,17 @@ inDegrees = {}
 for i in range(maxID):
 	outDegrees[i] = 0
 	inDegrees[i] = 0		
-
-
-with open(sys.argv[1],'rb') as infile:
-    
+with open(f,'rb') as infile:	    
 	for chunk in iter((lambda:infile.read(8)),''):
-	        src = struct.unpack('I', chunk[0:4])[0]
+		src = struct.unpack('I', chunk[0:4])[0]
        	        tgt = struct.unpack('I', chunk[4:8])[0]
-	        if src < maxID:
-        		outDegrees[src] = outDegrees[src] + 1
+		if src < maxID:
+			outDegrees[src] = outDegrees[src] + 1
 		if tgt < maxID:
-           		inDegrees[tgt] = inDegrees[tgt] + 1
+	   		inDegrees[tgt] = inDegrees[tgt] + 1
 
-print "outDegrees=" + outDegrees
-print "inDegrees=" + outDegrees
+print "OUT DEGREES EXPECTED"
+print outDegrees
+print "IN DEGREES EXPECTED"
+print inDegrees
 
--- trials/scripts/read_degree.py
+++ trials/scripts/read_degree.py
@@ -1,19 +1,15 @@
-
 import sys
 import struct
 
-
 maxID = 10
 degrees = {}
-i = 0
-with open(sys.argv[1],'rb') as infile:
-    
-	for chunk in iter((lambda:infile.read(8)),''):
-	        src = struct.unpack('I', chunk[0:4])[0]
-		if src < maxID:
-		     degrees[src] = struct.unpack('I', chunk[4:8])[0]
-	     
-
+src = 0
+with open(f,'rb') as infile:
+	for chunk in iter((lambda:infile.read(8)),''):			
+	        degrees[src] = struct.unpack('L', chunk[0:8])[0]
+		src = src + 1
+		if src >= maxID:
+			break;
 print degrees
 
 
