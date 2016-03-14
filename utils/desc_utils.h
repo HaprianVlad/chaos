882085ac0d0010248a4c13e1c412eac9f67cb813
VladH <vlad.haprian@epfl.ch>
2016-03-14 15:49:25 +0100

    modif

--- utils/desc_utils.h
+++ utils/desc_utils.h
@@ -54,8 +54,7 @@ static void init_partitions_offsets(const std::string &partitions_file_name) {
     boost::property_tree::ini_parser::read_ini(partitions_file_name, pt_partitions);
   }
   catch (...) {
-    BOOST_LOG_TRIVIAL(fatal) << "Unable to read partitions property file";
-    BOOST_LOG_TRIVIAL(fatal) << partitions_file_name;
+    BOOST_LOG_TRIVIAL(fatal) << "Unable to read partitions property file: "<< partitions_file_name;
     exit(-1);
   }
 }
