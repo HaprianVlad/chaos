d50280b3a6b627c85dc841024c5941460309bad2
VladH <vlad.haprian@epfl.ch>
2016-03-14 15:04:04 +0100

    modif

--- new_partitions_chaos/scripts/launchers/generate_graph.sh
+++ new_partitions_chaos/scripts/launchers/generate_graph.sh
@@ -3,7 +3,7 @@
 scale=$1
 edges=$((2 ** ($scale + 4)))
 export LD_LIBRARY_PATH="/usr/local/lib/:$LD_LIBRARY_PATH"
-/media/ssd/hpgp/branches/slipstream/generators/rmat --scale $(($scale)) --edges $edges --xscale_interval 1 --xscale_node 0 --name $2 --symmetr$
+/media/ssd/hpgp/branches/slipstream/generators/rmat --scale $(($scale)) --edges $edges --xscale_interval 1 --xscale_node 0 --name $2 --symmetr$ > ~/chaos_sandbox/graph
 
 
 
