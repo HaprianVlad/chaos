ec4c56f0bf62efa76d40c3a0cbbb8b5a686d1f15
VladH <vlad.haprian@epfl.ch>
2016-03-14 14:28:45 +0100

    bug fixed on split partition compile

--- new_partitions_chaos/scripts/python/create_new_partitions.py
+++ new_partitions_chaos/scripts/python/create_new_partitions.py
@@ -67,8 +67,8 @@ def printPartitionDetails(partitions):
 def printResults(results, fileName, c1, c2):
 	with open(fileName, 'w') as f:
 		f.write("[Partitions]" + '\n')
-		f.write("sum_out_degrees_for_new_super_partition+" str(c1))
- 		f.write("max_edges_per_new_super_partition+" str(c1))
+		f.write("sum_out_degrees_for_new_super_partition=" + str(c1))
+ 		f.write("max_edges_per_new_super_partition=" + str(c1))
 		f.write("number_of_new_super_partitions=" + str(len(results)) + '\n')
    		for v in range(len(results)):
 			f.write("P" + v + "=" + str(resulst[v]) + '\n') 
