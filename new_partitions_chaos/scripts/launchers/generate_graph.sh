d9a495ebb0569241b4e85ce8471b3bb3cc436372
root <root@dco-node137.dco.ethz.ch>
2016-03-14 15:27:17 +0100

    scripts modified

--- new_partitions_chaos/scripts/launchers/generate_graph.sh
+++ new_partitions_chaos/scripts/launchers/generate_graph.sh
@@ -3,7 +3,8 @@
 scale=$1
 edges=$((2 ** ($scale + 4)))
 export LD_LIBRARY_PATH="/usr/local/lib/:$LD_LIBRARY_PATH"
-/media/ssd/hpgp/branches/slipstream/generators/rmat --scale $(($scale)) --edges $edges --xscale_interval 1 --xscale_node 0 --name $2 --symmetr$ > ~/chaos_sandbox/graph
-
+/media/ssd/hpgp/branches/slipstream/generators/rmat --scale $(($scale)) --edges $edges --xscale_interval 1 --xscale_node 0 --name $2 --symmetric 
+mv $2 ~/chaos_sandbox/graph/
+rm -f *.ini
 
 
--- object_files/block_mem.d
+++
@@ -1,2 +0,0 @@
-object_files/block_mem.o: utils/block_mem.cpp utils/memory_utils.h \
- utils/boost_log_wrapper.h
