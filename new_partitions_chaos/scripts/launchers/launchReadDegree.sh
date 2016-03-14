570a1c6a93420d561c0fb41ab8cab3cf3f47229a
root <root@dco-node137.dco.ethz.ch>
2016-03-14 15:45:24 +0100

    scripts modified

--- new_partitions_chaos/scripts/launchers/launchReadDegree.sh
+++ new_partitions_chaos/scripts/launchers/launchReadDegree.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
-((!$#)) && echo No arguments supplied! Expect in or out && exit 1
+((!$#)) && echo No arguments supplied! Expect in_degree_rmat or out_degree_rmat && exit 1
 
-python ../python/read_degree.py  ~/chaos_sandbox/$1/stream.0.0.0 >  ~/chaos_sandbox/results/$1Degree.txt
+python ../python/read_degree.py  ~/chaos_sandbox/$1/stream.0.0.0 >  ~/chaos_sandbox/results/$1.txt
 
 
 
