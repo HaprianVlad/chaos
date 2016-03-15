195c32e8aa4ba02f84ad00aa52a58ad42ec7242a
root <root@dco-node137.dco.ethz.ch>
2016-03-15 18:27:13 +0100

    work in progess

--- new_partitions_chaos/scripts/launchers/launchReadDegree.sh
+++ new_partitions_chaos/scripts/launchers/launchReadDegree.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
-((!$#)) && echo No arguments supplied! Expect in_degree_rmat or out_degree_rmat && exit 1
+(($# != 3)) && echo No arguments supplied! Expect: in_degree_rmat or out_degree_rmat, number_of partitions, number_of_vertices_per_partitions && exit 1
 
-python ../python/read_degree.py  ~/chaos_sandbox/$1/stream.0.0.0 >  ~/chaos_sandbox/results/$1.txt
+python ../python/read_degree.py  ~/chaos_sandbox/$1/stream.0.0.0 $2 $3 >  ~/chaos_sandbox/results/$1.txt
 
 
 
--- new_partitions_chaos/scripts/python/read_degree.py
+++ new_partitions_chaos/scripts/python/read_degree.py
@@ -3,12 +3,15 @@ import struct
 
 maxID = 10
 degrees = {}
-src = 0
+offset = 0
 f = sys.argv[1]
+p = sys.argv[2]
+vp = sys.argv[3]
+ (i % vp) * p + i / vp
 with open(f,'rb') as infile:
 	for chunk in iter((lambda:infile.read(8)),''):			
 	        degrees[src] = struct.unpack('L', chunk[0:8])[0]
-		src = src + 1
+		offset = offset + 1
 		if src >= maxID:
 			break;
 print degrees
