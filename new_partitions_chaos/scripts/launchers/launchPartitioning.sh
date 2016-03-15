3ec6a4b4b43b28ca775d19e4112d83f0dd146ef2
VladH <vlad.haprian@epfl.ch>
2016-03-15 19:32:40 +0100

    work

--- new_partitions_chaos/scripts/launchers/launchPartitioning.sh
+++ new_partitions_chaos/scripts/launchers/launchPartitioning.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
-(($# != 4)) && echo "No arguments supplied!Expect: <out degrees folder name: out_degree_rmat24> <sum of Out degrees>  <max edges per partition> <result file>" && exit 1
+(($# != 6)) && echo "No arguments supplied!Expect: <out degrees folder name: out_degree_rmat24> <sum of Out degrees>  <max edges per partition> <result file> <number of partitions> <vertices per partition>" && exit 1
 
 cat ~/chaos_sandbox/$1/stream* >> ~/chaos_sandbox/$1/stream.merge
-python ../python/create_new_partitions.py ~/chaos_sandbox/$1/stream.merge $2 $3 $4
+python ../python/create_new_partitions.py ~/chaos_sandbox/$1/stream.merge $2 $3 $4 $5 $6
 rm  -f ~/chaos_sandbox/$1/stream.merge
 
 
