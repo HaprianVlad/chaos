47cec6d7702196e6de2c5aad4a507d06253f6f19
VladH <vlad.haprian@epfl.ch>
2016-03-15 18:33:21 +0100

    read degree changed

--- new_partitions_chaos/scripts/python/read_degree.py
+++ new_partitions_chaos/scripts/python/read_degree.py
@@ -7,13 +7,19 @@ offset = 0
 f = sys.argv[1]
 p = sys.argv[2]
 vp = sys.argv[3]
- (i % vp) * p + i / vp
+
+
 with open(f,'rb') as infile:
 	for chunk in iter((lambda:infile.read(8)),''):			
-	        degrees[src] = struct.unpack('L', chunk[0:8])[0]
 		offset = offset + 1
-		if src >= maxID:
+		v_id = getVertexId(offset, p, vp)
+		degrees[v_id] = struct.unpack('L', chunk[0:8])[0]
+		if v_id >= maxID:
 			break;
 print degrees
 
-
+def getVertexId(offset, p, vp): 
+	i=0
+	while (((i % vp) * p + i / vp) != i):
+		i+=1
+	return i 
