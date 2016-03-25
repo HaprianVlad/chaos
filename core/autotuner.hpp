83528e718f836b33194846e321b2da0f335a6454
VladH <vlad.haprian@epfl.ch>
2016-03-25 10:08:17 +0100

    modif added

--- core/autotuner.hpp
+++ core/autotuner.hpp
@@ -544,6 +544,7 @@ namespace x_lib {
 
         // should the super_partition where the vertex key is
         static unsigned long map(unsigned long key) {
+            BOOST_LOG_TRIVIAL(info) << "PARTITIONING::OLD " << configuration::old_partitioning_mode;
             return configuration::old_partitioning_mode ? map_internal_old(key) : map_internal_new(key) ;
         }
     };
