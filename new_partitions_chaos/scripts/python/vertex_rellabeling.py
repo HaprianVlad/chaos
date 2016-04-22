3161683eaae7de305b47c8a15be52fbc74cee287
VladH <vlad.haprian@epfl.ch>
2016-04-22 12:57:35 +0200

    work on vertex rellabeling

---
+++ new_partitions_chaos/scripts/python/vertex_rellabeling.py
@@ -0,0 +1,35 @@
+import sys
+import struct
+from numpy import np
+
+def main(argv):
+	in_graph = sys.argv[1]
+	scale = sys.argv[2]
+	out_graph = sys.argv[3]
+
+	permutation = getRandomPermutation(scale)
+
+	with open(in_graph,'rb') as infile:
+		with open(out_graph,'wb') as outfile:	    
+			for chunk in iter((lambda:infile.read(12)),''):
+				src = struct.unpack('I', chunk[0:4])[0]
+       		        	tgt = struct.unpack('I', chunk[4:8])[0]
+			
+				new_src = struct.pack('I', permutation[src])[0]
+				new_tgt = struct.pack('I', permutation[tgt])[0]
+				
+				outfile.write(new_src)
+				outfile.write(new_tgt)
+
+			
+
+def getRandomPermutation(scale):
+	return np.random.permutation(scale)
+
+
+
+if __name__ == "__main__":
+	if len (sys.argv) != 4:
+		print "Usage: python vertex_rellabeling.py <graph in file> <scale> <graph out file>"
+	else :
+		main(sys.argv[1:])
