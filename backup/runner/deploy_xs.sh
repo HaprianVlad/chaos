a2d6846acde8d556f2b03dbf87ef129efcb8f5bd
root <root@dco-node137.dco.ethz.ch>
2016-04-23 23:35:54 +0200

    fixed deploy 3

--- backup/runner/deploy_xs.sh
+++ backup/runner/deploy_xs.sh
@@ -10,12 +10,12 @@ clush -w dco-node[137-144] "mkdir -p /media/${device}/hpgp/branches"
 rm -f chaos.zip 
 zip -r chaos.zip ~/chaos
 clush -w dco-node[137-144] "rm -rf /media/${device}/hpgp/branches/slipstream*"
-for i in `seq 137 144`; do scp chaos.zip dco-node$i:/media/${device}/hpgp/branches/.; done
+for i in `seq 137 144`; do scp ~/runner/chaos.zip dco-node$i:/media/${device}/hpgp/branches/.; done
 clush -w dco-node[137-144] "cd /media/${device}/hpgp/branches/ && unzip chaos && mv chaos slipstream && cd slipstream && mkdir -p bin && mkdir -p object_files && make clean && make"
 
 rm -f helpers.zip
 zip -r helpers.zip ~/helpers
-for i in `seq 137 144`; do scp helpers.zip dco-node$i:/media/${device}/.; done
+for i in `seq 137 144`; do scp ~/runner/helpers.zip dco-node$i:/media/${device}/.; done
 clush -w dco-node[137-144] "cd /media/${device} && unzip helpers.zip"
 
 done
