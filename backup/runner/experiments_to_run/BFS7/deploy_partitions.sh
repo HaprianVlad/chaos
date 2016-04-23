88392f5ebd148a6a466cd71a489277bccc4efdf9
VladH <vlad.haprian@epfl.ch>
2016-04-23 20:44:48 +0200

    some modifs

--- backup/runner/experiments_to_run/BFS7/deploy_partitions.sh
+++ backup/runner/experiments_to_run/BFS7/deploy_partitions.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 command=~/runner/deploy_partitioning_files.sh
-sh $command 28 8 partitionFile 0
+sh $command 28 8 0 partitionFile
--- backup/runner/experiments_to_run/PR9/deploy_partitions.sh
+++ backup/runner/experiments_to_run/PR9/deploy_partitions.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 command=~/runner/deploy_partitioning_files.sh
-sh $command 28 8 partitionFile 1
+sh $command 28 8 1 partitionFile
