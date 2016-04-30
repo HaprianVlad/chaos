b4c338cd2b962073d974618e9ce4f4abc057c589
root <root@dco-node137.dco.ethz.ch>
2016-04-30 15:22:35 +0200

    some modifs

--- new_partitions_chaos/scripts/launchers/generate_graph.sh
+++ new_partitions_chaos/scripts/launchers/generate_graph.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
-(($#!=2)) && echo Need scale and output name! && exit 1
+(($#!=2)) && echo Need scale and directed! && exit 1
 scale=$1
 edges=$((2 ** ($scale + 4)))
 export LD_LIBRARY_PATH="/usr/local/lib/:$LD_LIBRARY_PATH"
-/media/ssd/hpgp/branches/slipstream/generators/rmat --scale $(($scale)) --edges $edges --xscale_interval 1 --xscale_node 0 --name $2 --symmetric 
-mv $2 ~/chaos_sandbox/graph/
+if [ $2 == 0 ]; then
+	name=rmat$1
+	/media/ssd/hpgp/branches/slipstream/generators/rmat --scale $(($scale)) --edges $edges --xscale_interval 1 --xscale_node 0 --name $name --symmetric 
+else
+	name=rmat$1_directed	
+	/media/ssd/hpgp/branches/slipstream/generators/rmat --scale $(($scale)) --edges $edges --xscale_interval 1 --xscale_node 0 --name $name
+fi
+mv $name ~/chaos_sandbox/graph/
 rm -f *.ini
 
 
