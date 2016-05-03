fb6bb17fd28dc6e57ee4f8767708212d3b032e43
VladH <vlad.haprian@epfl.ch>
2016-05-04 00:12:16 +0200

    fix

--- backup/runner/experiments_to_run/BFS_RMAT_24_TEST/deploy_partitions.sh
+++ backup/runner/experiments_to_run/BFS_RMAT_24_TEST/deploy_partitions.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 command=~/runner/deploy_partitioning_files.sh
-sh $command 28 8 0 ~/runner/experiments_to_run/BFS_RMAT_24_TEST/partitionFile
+sh $command 24 8 0 ~/runner/experiments_to_run/BFS_RMAT_24_TEST/partitionFile
