1cac344b69b54e5fbe84884d0203b06ec97e8616
VladH <vlad.haprian@epfl.ch>
2016-05-06 15:37:40 +0200

    grid partitioning in progress

--- backup/runner/deploy_grid_partitions.sh
+++ backup/runner/deploy_grid_partitions.sh
@@ -9,14 +9,14 @@ TARGET="dco-node[137-144]"
 for i in `seq 137 144`; do 
 index=$(($i-137))
 	if [ $2 -eq 0 ]; then
-		cat /media/ssd/grid/grid_partitions_$1/stream.2.$index.* >> s.2.$index.0 
+		cat /media/ssd/grid/grid_partitions_$1/stream.2.$index.* >> /media/ssd/grid/grid_partitions_$1/s.2.$index.0 
 	
 	else 
 
-		cat /media/ssd/grid/grid_partitions_$1/stream.2.*.$index >> s.2.$index.0 
+		cat /media/ssd/grid/grid_partitions_$1/stream.2.*.$index >> /media/ssd/grid/grid_partitions_$1/s.2.$index.0 
 	fi
 scp /media/ssd/grid/grid_partitions_$1/s.2.$index.0 dco-node$i.dco.ethz.ch:/media/ssd/stream.2.$index.0;
-rm -f s.2.$index.0 
+rm -f /media/ssd/grid/grid_partitions_$1/s.2.$index.0 
 
 done
 
