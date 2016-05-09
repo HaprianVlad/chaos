1466c2816362e84482951620e7652bb9822d5fef
VladH <vlad.haprian@epfl.ch>
2016-05-09 14:41:30 +0200

    work

--- new_partitions_chaos/scripts/launchers/launchGridPartitioning.sh
+++ new_partitions_chaos/scripts/launchers/launchGridPartitioning.sh
@@ -3,8 +3,13 @@
 
 (($# != 2)) && echo "No arguments supplied! Expect: <graph> <partition file> <row partitioning>" && exit 1
 
-rm -rf /media/ssd/grid/grid_partitions_$1
-mkdir /media/ssd/grid/grid_partitions_$1
+if [ $3 -eq 1 ]; then
+  name="row"
+else
+  name="column"
+fi 
+rm -rf /media/ssd/grid/grid_partitions_$1_$name
+mkdir /media/ssd/grid/grid_partitions_$1_$name
 pypy ../python/grid_partitioning.py ~/chaos_sandbox/graph/$1 $2 $1 $3
 
 
