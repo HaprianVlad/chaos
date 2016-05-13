be0d69bb9553cead7b8b7154be4a9ee926dc1c72
VladH <vlad.haprian@epfl.ch>
2016-05-13 15:02:54 +0200

    work on grid

--- new_partitions_chaos/scripts/launchers/launchGridDetails.sh
+++ new_partitions_chaos/scripts/launchers/launchGridDetails.sh
@@ -3,7 +3,7 @@
 
 (($# != 2)) && echo "No arguments supplied! Expect: <graph> <partition file>" && exit 1
 
-pypy ../python/grid_partitioning.py /media/ssd/generated_graphs/$1 $2 
+pypy ../python/grid_partitioning_details.py /media/ssd/generated_graphs/$1 $2 
 
 
 
