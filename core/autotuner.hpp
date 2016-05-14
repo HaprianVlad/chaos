d71641129a581e9402119e7c426a7e480aa511e9
VladH <vlad.haprian@epfl.ch>
2016-05-14 12:35:23 +0200

    work on grid

--- core/autotuner.hpp
+++ core/autotuner.hpp
@@ -773,10 +773,11 @@ namespace x_lib {
         }
 
         static bool additional_computation_needed() {
-            if (grid_partitioning && !init_phase) {
+            return true;
+            /*if (grid_partitioning && !init_phase) {
                 BOOST_ASSERT_MSG(cached_super_partition != -1, "Super_partition positioning lost");
             }
-            return !grid_partitioning || init_phase || cached_super_partition == -1;
+            return !grid_partitioning || init_phase || cached_super_partition == -1;*/
         }
 
     };
