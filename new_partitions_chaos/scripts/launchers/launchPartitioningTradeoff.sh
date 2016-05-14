0909a69ca780a39410fd0cabd191924101363009
VladH <vlad.haprian@epfl.ch>
2016-05-14 20:54:05 +0200

    work on presentation

--- new_partitions_chaos/scripts/launchers/launchPartitioningTradeoff.sh
+++ new_partitions_chaos/scripts/launchers/launchPartitioningTradeoff.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 (($# != 10)) && echo "No arguments supplied!Expect: <out degrees folder name: out_degree_rmat24> <vertex_state_size>  <edge_state_size> <scale> <alpha> <undirected> <number of partitions> <result file> <number of partitions> <vertices per partition> " && exit 1
 
-cat /media/ssd/degrees/$1/stream.0.* >> media/ssd/degrees/$1/stream.merge
+cat /media/ssd/degrees/$1/stream.0.* >> /media/ssd/degrees/$1/stream.merge
 pypy ../python/create_new_partitions_tradeoff.py /media/ssd/degrees/$1/stream.merge $2 $3 $4 $5 $6 $7 $8 $9 ${10}
 rm  -f /media/ssd/degrees/$1/stream.merge
 
